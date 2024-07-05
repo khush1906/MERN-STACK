561b571cc5284275e3af40f92d71bcd24ac1d8a0
khush1906 <155256455+khush1906@users.noreply.github.com>
2024-07-06 00:01:17 +0530

    new repo created

---
+++ javascript/hello.java/hello.js
@@ -0,0 +1,8 @@
+
+
+//console.log('Hello World');
+//prompt("What is your name?")
+var myName ="khushboo";
+//alert(myName);
+
+alert("My Nmae is " +myName+ " hello everyone i am new " );
---
+++ javascript/hello.java/tp.js
@@ -0,0 +1,4 @@
+var gameLevel=1;
+gameLevel=2;
+gameLevel=3;
+alert("curentlly you game level is " + gameLevel );
